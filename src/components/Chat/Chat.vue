057c25b4968134c25dd8aa717a6a7d45ecaf1a0b
zzahraJ <r0883814@student.thomasmore.be>
2023-11-29 16:40:56 +0100

    feat: add the comment section

--- src/components/Chat/Chat.vue
+++ src/components/Chat/Chat.vue
@@ -1,47 +1,55 @@
 <script setup>
-    // import ref
+    //import ref
     import { ref, reactive, onMounted } from 'vue';
 
-    // {{ }} gebruik je om iets af te printen
     let message = ref(""); // kan reflecteren naar de front-end gebruik int, string, boolean
-    let allMessages = reactive({
+    let allMessages= reactive({
         data: ["Heel", "Even", "Simple","Doen"],
     }); 
 
+    const name = "ZAHRA";
+
     // function onMounted
-    onMounted(async() => {
-        try {
-            const response = await fetch("https://lab5-p379.onrender.com/api/v1/messages/");
-            const data = await response.json();
-            allMessages.data = data;
-        } catch (error) {
-            console.error("error:", error); 
+    onMounted(async () => {
+        const response = await fetch("https://lab5-p379.onrender.com/api/v1/messages/");
+        const data = await response.json();
+        allMessages.data = data;
+    }); 
+
+    //function SendMessage
+    const SendMessage = async () => {
+        const newMessage ={
+            user: name,
+            text: message.value,
         }
+    const response = await fetch("https://lab5-p379.onrender.com/api/v1/messages/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newMessage),
     });
 
-    // function sendMessage
-    const SendMessage = () => {
-        allMessages.data.push(message.value);
-        message.value = ""; //.value moet je gebruiken als je met ref werkt, heeft niks te maken met het tekstvak
-    };
-    
+    allMessages.data.push(newMessage);// new message toevoegen aan de array
+    message.value = ""; //.value moet je gebruiken als je met ref werkt, maakt het tekstvak terug leeg
+};
 
 </script>
 
 <template>
-  <div>
-    <ul>
-        <li v-for="m in allMessages.data">{{ m }}</li> 
-    </ul>
-
     <div>
-        <input v-model="message" type="text" placeholder="" />
-        <button @click="SendMessage">Send</button>
+        <ul>
+            <li v-for="m in allMessages.data" :key="m.id">{{ m.user}}: {{ m.text }}</li> 
+        </ul>
+        <div>
+            <input v-model="message" type="text" placeholder="">
+            <button @click="SendMessage">Send</button>
+        </div>
     </div>
-
-  </div>
 </template>
 
-<style scoped> 
+
+<style scoped>
 
 </style>
+
