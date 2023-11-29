6af788cc97a91f2c352d57f7f0c4f8c13e6924da
zzahraJ <r0883814@student.thomasmore.be>
2023-11-29 16:47:02 +0100

    feat: fix the video

--- src/components/Video/VideoPlayer.vue
+++ src/components/Video/VideoPlayer.vue
@@ -1,53 +1,23 @@
-<!-- <script setup>
-    import { ref, reactive, onMounted } from 'vue'; 
-
-    let videoUrl = ref(""); // met : ga je binden als dit verandert gaat de video ook veranderen
-    let videos = reflective({ 
-        data: [],
-    });
-
-    onMounted(() => {
-        fetch("https://api.jsonbin.io/v3/b/6548ef9954105e766fcc2c15")
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            videos.data = data.record.videos;
-            videoUrl.value = videos.data[0].video; // hier moet je de counter + 1 doen om de nieuwe video te krijgen
-        });
-    });
-
-</script>
-
-<template>
-  <div>
-    <video :src="videoUrl" controls autoplay muted loop></video>
-  </div>
-</template>
-
-<style scoped> 
-
-</style> -->
-
 <script setup>
     import { ref, reactive, onMounted } from 'vue';
 
     //define emits
-    const emit = defineEmits(['update:videoDescription']);
+    const emits = defineEmits(["update:videoDescription"]); // deze link aanpassen met je eigen
     
-    let videoUrl = ref("");
+    let videoUrl = ref(""); // met : ga je binden als dit verandert gaat de video ook veranderen
     let videos =reactive({
         data:[],
     });
 
     onMounted(() => {
-        fetch("https://api.jsonbin.io/v3/b/6548ef9954105e766fcc2c15") // deze link aanpassen met je eigen
+        fetch("https://api.jsonbin.io/v3/b/6548ef9954105e766fcc2c15")
         .then((res) => res.json())
         .then((data) => {
             videos.data = data.record.videos;
-            videoUrl.value = videos.data[0].videoUrl;
+            videoUrl.value = videos.data[0].video;
 
-            //emit
-            emit('update:videoDescription', videos.data[0].description);
+            // emit event
+            emits("update:videoDescription",videos.data[0].description);
         });
     });
 </script>
